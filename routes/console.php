f2acfe6c59437d607fcced7273c7632c931baebf
Janaksinh Parmar <68726506+janakparmar9491@users.noreply.github.com>
2020-12-12 21:08:50 +0530

    Display all questions

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('clear:all', function () {
+    Artisan::call('route:cache');
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('optimize');
+    $this->comment('Clearing Cache,View,Route,Config, reoptimize the class loader.');
+})->describe('Clearing Cache,View,Route,Config,Reoptimize the class loader.');
